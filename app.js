8d1495553120a2edf3e3e7c85f88bdeb4b09b78a
Zvi Haffner <zvihaffner@gmail.com>
2025-04-27 17:36:39 +0100

    Creates and tests endpoint to retrieve task by id with error handling

--- app.js
+++ app.js
@@ -4,6 +4,23 @@ const app = express();
 
 app.use("/api", apiRouter);
 
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad Request" });
+  } else next(err);
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ msg: "Internal Server Error" });
+});
+
 // app.all("*", (req, res) => {
 //   res.status(404).send({ msg: "Route not found" });
 // });
--- models/tasks.models.js
+++ models/tasks.models.js
@@ -3,3 +3,18 @@ const db = require("../db/connection");
 exports.fetchAllTasks = () => {
   return db.query("SELECT * FROM tasks");
 };
+
+exports.fetchTaskById = (id) => {
+  return db
+    .query("SELECT * FROM tasks WHERE id = $1", [id])
+    .then(({ rows }) => {
+      const task = rows[0];
+      if (!task) {
+        return Promise.reject({
+          status: 404,
+          msg: `No task found for ID: ${id}`,
+        });
+      }
+      return task;
+    });
+};
